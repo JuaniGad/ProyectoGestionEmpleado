c2f3fe0deb7bc1952989842d3107434569a2d4e7
Juan Gadpen <juangadpen@hotmail.com>
2023-01-13 01:57:16 -0300

    Actulizacion para uso de Scroll

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,23 +12,31 @@ function App() {
   return (
     <div className="bg-black w-full overflow-hidden">
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-        <div className={`${styles.boxWidth}`}>
+        <div className={`${styles.boxWidth} `}>
           <NavBar />
         </div>
       </div>
 
       <div className={`bg-black ${styles.flexStart}`}>
-        <div className={`${styles.boxWidth}`}>
+        <div id="inicio"className={`${styles.boxWidth}`}>
           <Info />
         </div>
       </div>
 
       <div className={`bg-black ${styles.paddingX} ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
+          <section id="clientes">
           <Clientes/>
+          </section>
+          <section id="preguntas_frecuentes">
           <PreguntasFrecuentes/>
+          </section>
+          <section id="contacto">
           <Contacto/>
+          </section>
+          <section id="enlaces">
           <Enlaces/>
+          </section>
           <Footer/>
 
         </div>
